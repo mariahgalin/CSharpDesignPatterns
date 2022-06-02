70bdcb99da8d56b99c024d66cd5773d31f3594f8
María Montoya <mariag.gmontoya@gmail.com>
2022-06-02 17:44:35 -0500

    Section2 POO T2

---
+++ Introduccion/Introduccion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person maria = new Person("Maria", 24, "Mexicana");
+
+            Console.WriteLine(maria.Show());
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string name;
+        public int age;
+        public string nationality;
+        public Person(string name_, int age_, string nationality_)
+        {
+            name = name_;
+            age = age_;
+            nationality = nationality_;
+        }
+
+        public string Show()
+        {
+            return name + " " + age + " " + nationality;
+        }
+    }
+}
