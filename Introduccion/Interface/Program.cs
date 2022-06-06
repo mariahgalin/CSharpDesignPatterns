7a1f1bf8187fb792cd8cf175bcd3b9bf36b4b291
María Montoya <mariag.gmontoya@gmail.com>
2022-06-06 17:39:11 -0500

    Section2 POO T4 Interface

---
+++ Introduccion/Interface/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IVolador> aves = new List<IVolador>();
+
+            var pato1 = new Pato();
+            var pato2 = new Pato();
+            var pato3 = new Pato();
+            var gallina = new Gallina();
+
+            aves.Add(pato1);
+            aves.Add(pato2);
+            aves.Add(pato3);
+            aves.Add(gallina);
+
+            AVolar(aves);
+
+            Console.ReadLine();
+        }
+
+        static void AVolar(List<IVolador> aves)
+        {
+            foreach (var ave in aves)
+            {
+                ave.Vuela();
+            }
+        }
+
+        interface IVolador
+        {
+             void Vuela();
+
+        }
+
+        interface ICaminador
+        {
+            void Camina();
+        }
+
+        public class Pato : IVolador, ICaminador
+        {
+            public void Vuela()
+            {
+                Console.WriteLine("Pato vuela");
+            }
+
+            public void Camina()
+            {
+                Console.WriteLine("Pato camina");
+            }
+        }
+
+        public class Gallina : IVolador
+        {
+            public void Vuela()
+            {
+                Console.WriteLine("Gallina vuela");
+            }
+        }
+    }
+}
