163e103be095b7194ba222212e143613ce3a74af
María Montoya <mariag.gmontoya@gmail.com>
2022-06-07 17:31:42 -0500

    Section3 T5 Singleton

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var singleton = Singleton.Singleton.Instance;
+
+            var log = Singleton.Log.Instance;
+            log.Save("a");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        private Singleton()
+        {
+            
+        }
+    }
+}
