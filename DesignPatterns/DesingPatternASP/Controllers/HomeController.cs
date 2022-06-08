4a29748dfdee5383780a6a9571cba5177b104efc
María Montoya <mariag.gmontoya@gmail.com>
2022-06-08 17:50:00 -0500

    Section3 T6 Singleton in ASP

---
+++ DesignPatterns/DesingPatternASP/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DesingPatternASP.Models;
+using Tools;
+using Microsoft.Extensions.Options;
+using DesingPatternASP.Configuration;
+
+namespace DesingPatternASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptions<MyConfig> _config;
+
+        public HomeController (IOptions<MyConfig>config)
+        {
+            _config = config;            
+        }
+        public IActionResult Index()
+        {
+            Log.GetInstance(_config.Value.PathLog).Save("Entro a index");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Log.GetInstance("log.txt").Save("Entro a privacy");
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DesignPatterns/DesingPatternASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesingPatternASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
